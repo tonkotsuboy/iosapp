d36593792845d81f3915e08db46d2862eb9756d1
tonkotsuboy_com <t.kano.624@gmail.com>
2017-05-05 17:31:28 +0900

    add demo3

---
+++ demo3/demo3/demo3/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  demo3
+//
+//  Created by 鹿野壮 on 2017/05/05.
+//  Copyright © 2017年 鹿野壮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func returnToMain(segue:UIStoryboardSegue) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    }
+
+
+}
+
